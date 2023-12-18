9a588f0d1dddb9fd3d90c6bff6bb4e2917dd1369
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-12-18 18:53:48 +0530

    fixed navbar logo

--- Flex-hub/src/components/navbar/index.tsx
+++ Flex-hub/src/components/navbar/index.tsx
@@ -25,7 +25,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
             <div className={`${flexBetween} mx-auto w-5/6`}>
               <div className={`${flexBetween} w-full gap-16`}>
                 {/* LEFT SIDE */}
-                <img alt="logo" src={Logo} />
+                <img alt="logo" className="h-5 w-auto" src={Logo} />
     
                 {/* RIGHT SIDE */}
                 {isAboveMediumScreens ? (
--- Flex-hub/src/shared/ActionButton.tsx
+++ Flex-hub/src/shared/ActionButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 const ActionButton = ({children, setSelectedPage}: Props) => {
   return (
     <AnchorLink
-        className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
+        className="rounded-md bg-secondary-500 px-4 py-2 hover:bg-primary-500 hover:text-white"
         onClick={() => setSelectedPage(SelectedPage.ContactUs)}
         href={`#${SelectedPage.ContactUs}`}
     >
